099024975fae44e7fd981a402fd56ba60ac33dd9
Paloma S Ferreira <103579480+lomaferreira@users.noreply.github.com>
2023-06-04 22:03:07 -0300

    file removed

--- cigarro.py
+++
@@ -1,4 +0,0 @@
-cigarros=int(input("Quantos cigarros fuma por dia?\n"))
-anos=int(input("Quantos anos já fumou?\n"))
-reducaoDeVida= ((cigarros*10)*(anos*365))//1440
-print(f"Você já perdeu {reducaoDeVida} dias de vida")
